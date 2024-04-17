8a2a5c7f9b067922d4b21694fb0f6e2c1eb1ef0d
jsq1234 <mananprakash3078@gmail.com>
2024-04-17 06:05:43 +0530

    fix: added 'post-service' and 'reply-service' prefix to add the paths in the respective microservice

--- post-service/src/main/java/com/tuningbill/postservice/PostController.java
+++ post-service/src/main/java/com/tuningbill/postservice/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.tuningbill.postservice.dto.PostBody;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/post-service")
 public class PostController {
     private final PostService postService;
 
--- reply-service/src/main/java/com/tuningbill/replyservice/ReplyController.java
+++ reply-service/src/main/java/com/tuningbill/replyservice/ReplyController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.tuningbill.replyservice.dto.ReplyBody;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor    
+@RequestMapping("/reply-service")
 public class ReplyController {
     private final ReplyService replyService;
 
